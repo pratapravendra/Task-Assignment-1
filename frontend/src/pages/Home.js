5b0ca34f9c64852e5425b51b53fe3d5df4ffd89e
pratapravendra <119398201+pratapravendra@users.noreply.github.com>
2024-08-31 12:03:58 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -108,7 +108,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = "https://task-bsv6.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
